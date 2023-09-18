bafcf71be67070ef39bbc353ab7c717c1e94efbe
[Jorge Galindo] <[jorgegal0704@gmail.com]>
2023-09-18 10:06:24 -0600

    Comenzando primera sección del curso

---
+++ PythonAlejandroT/Elementos basicos/asignacionValores.py
@@ -0,0 +1,16 @@
+num1 = 10
+num2 = 6.7
+resultado = (num1 + num2) * 10 / 6
+
+
+print("El resultado es: ", resultado)
+
+#print(type(valor)) Type indica el tipo de dato de la variable que se está usando
+
+valor = 10
+
+print(valor)
+
+valor = "Jorge"
+
+print(valor)
